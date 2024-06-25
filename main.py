7f45a898774307c780117cdbc2644c1db472e9b7
pocem <misogrega21@gmail.com>
2024-06-25 13:14:57 +0200

    making the data clear, sending it to a spreadsheet

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
-google_forms_link = "https://docs.google.com/forms/d/e/1FAIpQLSfQZOv8d3v7wl6YuJ1y3E1sqYEZFDtWrTgc7uaZP2PTONxTeQ/viewform?usp=sf_link"
+google_forms_link = "https://docs.google.com/forms/d/e/1FAIpQLSfQZOv8d3v7wl6YuJ1y3E1sqYEZFDtWrTgc7uaZP2PTONxTeQ/viewform"
 rentals_url = "https://appbrewery.github.io/Zillow-Clone/"
 
 response = requests.get(rentals_url)
@@ -19,7 +22,6 @@ addresses = listings_group.find_all("address")
 
 list_of_addresses = [address.get_text(strip=True) for address in addresses]
 
-print(list_of_addresses)
 
 #list of links
 list_of_links = []
@@ -27,11 +29,52 @@ for link in listings:
     url = link["href"]
     list_of_links.append(url)
 
-print(list_of_links)
 
 #list of prices
 list_of_prices = []
 for listings_price in listings_prices:
-    list_of_prices.append(listings_price.get_text())
+    price_text = listings_price.get_text()
+    stripped_price = price_text.replace("+/mo", "").replace("/mo", "").replace("+ 1bd", "").strip()
+    list_of_prices.append(stripped_price)
 
-print(list_of_prices)
+#SELENIUM AUTOFILL
+driver = webdriver.Chrome()
+driver.get(google_forms_link)
+
+def input_address():
+    address_input = driver.find_element(By.XPATH,
+                                        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    address_input.send_keys(list_of_addresses[i])
+
+def input_price():
+    price_input = driver.find_element(By.XPATH,
+                                      '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price_input.send_keys(list_of_prices[i])
+
+def input_link():
+    link_input = driver.find_element(By.XPATH,
+                                     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link_input.send_keys(list_of_links[i])
+
+def submit():
+    submit_button = driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div/span')
+    submit_button.click()
+
+def submit_another():
+    submit_another_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+    submit_another_button.click()
+
+
+for i in range(len(list_of_prices)):
+    time.sleep(5)
+    input_address()
+    input_price()
+    input_link()
+    submit()
+    time.sleep(3)
+    submit_another()
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+Automated data sorting
+
+Scraping data from web using BeautifulSoup, sorting it in lists and automatically sending them to a spreadsheet using Selenium, where they are displayed with the necessary info
